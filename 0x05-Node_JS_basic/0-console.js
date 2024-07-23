49b926ec0e36a2ac21637380112bc20563c984f3
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2024-07-23 03:44:45 +0000

    console

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,15 @@
+/**
+ * Logs a message to the console.
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
+// The arrow function
+const displayMessage = (message) => console.log(message);
+
+/**
+ * Exports the displayMessage function.
+ * @module displayMessage
+ * @function
+ */
+// The default module export.
+module.exports = displayMessage;
