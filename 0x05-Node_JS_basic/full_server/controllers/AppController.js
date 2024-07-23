90d09357bb7bf78d6bd607209d3c472581372232
OmarElsakka1 <s-omar.elsakka@zewailcity.edu.eg>
2024-07-23 22:56:05 +0000

    App Controller

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,11 @@
+/**
+ * The miscellaneous route.
+ */
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+}
+
+export default AppController;
+module.exports = AppController;
